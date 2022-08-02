948b40e152f4f672664d5a5e241de67df3f1ccb1
chae-hwa <ihj0519@naver.com>
2022-08-02 23:47:07 +0900

    들어오는 요청에 맞는 매핑 정보를 찾는다

--- src/main/java/com/ll/exam/Ut.java
+++ src/main/java/com/ll/exam/Ut.java
@@ -1,15 +1,72 @@
-package com.ll.exam;
+package com.ll.exam.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Ut {
-    public static class cls{
-        public static <T> T newObj(Class<T> cls, T defaultValue){ // 클래스가 들어오면
+    public static Map<String, Object> mapOf(Object... args) {
+        int dataSize = args.length / 2;
+
+        Map<String, Object> map = new LinkedHashMap<>();
+
+        for (int i = 0; i < dataSize; i++) {
+            int keyIndex = i * 2 + 0;
+            int valueIndex = i * 2 + 1;
+
+            String key = (String) args[keyIndex];
+            Object value = args[valueIndex];
+
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+    public static class json {
+        private static final ObjectMapper om;
+
+        static {
+            om = new ObjectMapper();
+        }
+
+        public static String toStr(Object obj, String defaultValue) {
             try {
-                return cls.getDeclaredConstructor().newInstance(); // 그 안의 클래스 객체를 생성해준다.
+                return om.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+
+        public static <T> T toObj(String jsonStr, Class<T> cls, T defaultValue) {
+            try {
+                return om.readValue(jsonStr, cls);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+
+        public static <T> T toObj(String jsonStr, TypeReference<T> typeReference, T defaultValue) {
+            try {
+                return om.readValue(jsonStr, typeReference);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+    }
+
+    public static class cls {
+
+        public static <T> T newObj(Class<T> cls, T defaultValue) {
+            try {
+                return cls.getDeclaredConstructor().newInstance();
             } catch (InstantiationException e) {
-                return defaultValue;  // 오류가 날 때마다 defaultValue로 반환
+                return defaultValue;
             } catch (IllegalAccessException e) {
                 return defaultValue;
             } catch (InvocationTargetException e) {
@@ -32,7 +89,23 @@ public class Ut {
 
             return new String(c);
         }
+
+        public static String beforeFrom(String str, String fromStr, int matchCount) {
+            StringBuilder sb = new StringBuilder();
+
+            String[] bits = str.split(fromStr);
+
+            for (int i = 0; i < matchCount; i++) {
+                sb.append(bits[i]);
+                if ( i + 1 < matchCount ) {
+                    sb.append(fromStr);
+                }
+            }
+
+            return sb.toString();
+        }
     }
+
     public static class reflection {
         public static <T> T getFieldValue(Object o, String fieldName, T defaultValue) {
             Field field = null;
@@ -46,7 +119,7 @@ public class Ut {
             field.setAccessible(true);
 
             try {
-                return (T)field.get(o);
+                return (T) field.get(o);
             } catch (IllegalAccessException e) {
                 return defaultValue;
             }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -4,6 +4,7 @@ import com.ll.exam.article.Repository.ArticleRepository;
 import com.ll.exam.article.Service.ArticleService;
 import com.ll.exam.article.controller.ArticleController;
 import com.ll.exam.home.controller.HomeController;
+import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
