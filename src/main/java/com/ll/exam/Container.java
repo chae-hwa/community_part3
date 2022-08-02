40466d58c902ae4a361b494f36061ffd5298bc1d
chae-hwa <ihj0519@naver.com>
2022-08-02 14:26:07 +0900

    articleController와 같은 1개만 필요한 객체를 싱글톤으로 작동

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -3,7 +3,12 @@ package com.ll.exam;
 import com.ll.exam.article.controller.ArticleController;
 
 public class Container {
+    private static ArticleController articleController;
+
+    static { // 클래스가 로딩될 때 1번만 실행
+        articleController = new ArticleController();
+    }
     public static ArticleController getArticleController() {
-        return new ArticleController();
+        return articleController; // 객체 재탕
     }
 }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -21,4 +21,12 @@ public class AppTest {
         assertThat(articleController).isNotNull();
     }
 
+    @Test
+    public void ioc_articleController__싱글톤(){ // 객체를 2번 달라 했을 때 그 객체가 동일해야 된다.
+        ArticleController articleController1 = Container.getArticleController();
+        ArticleController articleController2 = Container.getArticleController();
+
+        assertThat(articleController2).isEqualTo(articleController1);
+    }
+
 }
