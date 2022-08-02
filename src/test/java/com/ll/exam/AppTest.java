ce6629f374211e9b7e988207a09f72880d79a3b0
chae-hwa <ihj0519@naver.com>
2022-08-02 20:34:34 +0900

    Container.getObj 메서드에 제너릭 적용하여, 형변환 제거

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -17,30 +17,30 @@ public class AppTest {
 
     @Test
     public void ioc_articleController(){
-        ArticleController articleController = (ArticleController)Container.getObj(ArticleController.class);
+        ArticleController articleController = Container.getObj(ArticleController.class);
 
         assertThat(articleController).isNotNull();
     }
 
     @Test
     public void ioc_articleController__싱글톤(){ // 객체를 2번 달라 했을 때 그 객체가 동일해야 된다.
-        ArticleController articleController1 = (ArticleController)Container.getObj(ArticleController.class);
-        ArticleController articleController2 = (ArticleController)Container.getObj(ArticleController.class);
+        ArticleController articleController1 = Container.getObj(ArticleController.class);
+        ArticleController articleController2 = Container.getObj(ArticleController.class);
 
         assertThat(articleController2).isEqualTo(articleController1);
     }
 
     @Test
     public void ioc_homeController(){
-        HomeController homeController = (HomeController)Container.getObj(HomeController.class);
+        HomeController homeController = Container.getObj(HomeController.class);
 
         assertThat(homeController).isNotNull();
     }
 
     @Test
     public void ioc_homeController__싱글톤(){ // 객체를 2번 달라 했을 때 그 객체가 동일해야 된다.
-        HomeController homeController1 = (HomeController)Container.getObj(HomeController.class);
-        HomeController homeController2 = (HomeController)Container.getObj(HomeController.class);
+        HomeController homeController1 = Container.getObj(HomeController.class);
+        HomeController homeController2 = Container.getObj(HomeController.class);
 
         assertThat(homeController2).isEqualTo(homeController1);
     }
